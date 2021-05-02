a4df292f0e5e54d4b287a66abb322df78b0b1b2d
kamilkow1123 <22kamil.kow@gmail.com>
2021-05-03 00:03:21 +0200

    Basic setup

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { BrowserRouter, Route} from 'react-router-dom'
+
+
+
+function App() {
+  return (
+    <div className="App">
+      App
+    </div>
+  );
+}
+
+export default App;
